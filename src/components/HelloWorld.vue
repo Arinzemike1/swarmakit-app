2fa27e8027e1ef4ffd85bcadf657e509aa81f503
Arinze Ihemedu <drealzorix@gmail.com>
2024-12-12 17:42:21 +0100

    Initial setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <button>Learn more</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
